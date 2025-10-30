6a435e2c45cb8c5acf0814e5a500f4f05f8695fd
idwbyh <chaifordanila@gmail.com>
2025-10-30 15:31:52 +0400

    third commit

--- bot.py
+++ bot.py
@@ -5,11 +5,11 @@ import telebot
 BOT_TOKEN = os.getenv("BOT_TOKEN") or "сюда_временно_вставь_твой_токен_из_BotFather"
 bot = telebot.TeleBot(BOT_TOKEN)
 
-YES_PATTERN = re.compile(r'^(да+|дa+|da+|dа+)$', re.IGNORECASE)
+YES_PATTERN = re.compile(r'^(да+|дa+|da+|dа+|Да+|Da+|ДА+|DA)$', re.IGNORECASE)
 
 @bot.message_handler(func=lambda message: message.text and YES_PATTERN.match(message.text.strip()))
 def reply_yes(message):
-    bot.reply_to(message, "Согласен!")
+    bot.reply_to(message, "Пизда")
 
 @bot.message_handler(func=lambda message: True)
 def ignore_all(message):
